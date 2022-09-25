5de11a1cdd648d34262c899fdd7d1dcd358c1ea6
AnastasiqDimitrova <109695621+AnastasiqDimitrova@users.noreply.github.com>
2022-09-25 23:03:48 +0300

    Add project files.

---
+++ 04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04.SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int combinations = 0;
+            bool hasMagicNumberBeenFound = false;
+
+            for (int firstNumber = startingNumber; firstNumber <= endNumber; firstNumber++)
+            {
+                for (int secondNumber = startingNumber; secondNumber <= endNumber; secondNumber++)
+                {
+                    combinations++;
+                    if (firstNumber + secondNumber == magicNumber)
+                    {
+                        hasMagicNumberBeenFound = true;
+                        Console.WriteLine($"Combination N:{combinations} ({firstNumber} + {secondNumber} = {magicNumber})");
+                        break;
+                    }
+                }
+
+                if (hasMagicNumberBeenFound)
+                {
+                    break;
+                }
+            }
+            
+            if (!hasMagicNumberBeenFound)
+            {
+                Console.WriteLine($"{combinations} combinations - neither equals {magicNumber}");
+            }
+        }
+        
+    }
+}
